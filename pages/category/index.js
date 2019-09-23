2baea0e979f99df493aca4966664df4cc79d3d9d
unknown <1462296228@qq.com>
2019-09-23 19:29:00 +0800

    03-完成分类页左侧导航

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,26 @@
 // pages/category/index.js
+import { request } from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 选中添加的样式
+    currentIndex:0,
+    leftDatalist:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(){
+    this.getleftCategories()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 发送请求拿到数据
+  getleftCategories(){
+    request({
+      url:'/categories'
+    }).then((result)=>{
+      console.log(result)
+      // 先拿到第一层数据
+      const leftDatalist = result.data.message.map(v=>v.cat_name)
+      // 赋给定义的数组
+      this.setData({
+        leftDatalist,
+      })
+    })
   }
 })
--- request/index.js
+++ request/index.js
@@ -1,4 +1,15 @@
+
+// 定义加载次数
+let requestTimes = 0
+
 export const request = (params) =>{
+    // 当调用一次request就加载多次
+    requestTimes++
+    // 显示遮罩层
+    wx.showLoading({
+        title: "加载中",
+        mask: true,
+    });
     const baseUrl = "https://api.zbztb.cn/api/public/v1" 
     return new Promise((resolve,reject) =>{
         wx.request({
@@ -9,7 +20,11 @@ export const request = (params) =>{
             fail: (err) => {
                 reject(err)
             },
-            complete: () => {}
+            complete: () => {
+                // 判断requestTimes等于0就隐藏遮罩
+                requestTimes--
+                requestTimes===0 && wx.hideLoading();   
+            }
         });
           
     })
